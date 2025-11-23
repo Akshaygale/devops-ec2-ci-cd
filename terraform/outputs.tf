f558f07b80c5dfa3c3cfb09d246f33641c725090
Akshay Gale <90633510+Akshaygale@users.noreply.github.com>
2025-11-23 12:48:08 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "frontend_public_ip" {
+output "frontend_ip" {
   value = aws_instance.frontend.public_ip
 }
 
-output "backend_public_ip" {
+output "backend_ip" {
   value = aws_instance.backend.public_ip
 }
